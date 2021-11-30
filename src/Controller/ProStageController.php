43d855f2bf7ac00be31fcb9975784b9f37b95597
Aurélien Vantalon <avantalon@iutbayonne.univ-pau.fr>
2021-11-30 16:23:32 +0100

    tp2

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,12 +9,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/prostage", name="pro_stage")
+     * @Route("/", name="pro_stage")
      */
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
             'controller_name' => 'ProStageController',
+        ]);
+    }
+	
+	/**
+     * @Route("/stages/{id}", name="pro_stage_stages")
+     */
+	public function afficherStages($id): Response
+    {
+        return $this->render('pro_stage/pageStages.html.twig', [
+            'controller_name' => 'ProStageController', 'id' => $id
+        ]);
+    }
+	
+	/**
+     * @Route("/formations", name="pro_stage_formations")
+     */
+	public function afficherFormations(): Response
+    {
+        return $this->render('pro_stage/pageFormations.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+	
+	/**
+     * @Route("/entreprises", name="pro_stage_entreprise")
+     */
+	public function afficherEntreprises(): Response
+    {
+        return $this->render('pro_stage/pageEntreprises.html.twig', [
+            'controller_name' => 'ProStageController', 
         ]);
     }
 }
