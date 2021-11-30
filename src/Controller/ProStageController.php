bcf8327cb1d0fd38c50d931442f9fc69317b62a1
Aurélien Vantalon <avantalon@iutbayonne.univ-pau.fr>
2021-11-30 15:48:49 +0100

    route via route.yaml

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -8,9 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProStageController extends AbstractController
 {
-    /**
-     * @Route("/pro/stage", name="pro_stage")
-     */
+    
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
