5bf4e666b01e58ddf9a290015ac65fd8aea2d5b7
Aurélien Vantalon <avantalon@iutbayonne.univ-pau.fr>
2022-01-08 17:03:01 +0100

    ajout des données test

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,20 +5,59 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Entreprise;
+use App\Entity\Stage;
+use App\Entity\Formation;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
        
-        // $manager->persist($product);
-		$entrepriseDevWeb = new Entreprise();
-		$entrepriseDevWeb->setNom("ritoGamez");
-		$entrepriseDevWeb->setAdresse("30 rue des pigeon");
-		$entrepriseDevWeb->setActivite("jeux vidéo");
-		$entrepriseDevWeb->setUrlSite("http://tito");
+		$faker = \Faker\Factory::create('fr_FR');
+		$tabEntreprise[];
+		//$stageRito1 = new Stage(
 		
-		$manager->persist($entrepriseDevWeb);
-        $manager->flush();
+		for($i =0 ; $i < 5 ; $i = $i +1){
+			$entreprise = new Entreprise();
+			$nomEnt = $faker -> company;
+			$entreprise->setNom($nomEnt);
+			$entreprise->setAdresse($faker->address);
+			$entreprise->setActivite($faker->realText($maxNbChars = 25, $indexSize = 2));
+			$entreprise->setUrlSite("http://$nomEnt.fr");
+			
+			$manager->persist($entreprise);
+			$tabEntreprise[$i] = $entreprise;
+			
+			
+		}
+		
+		$tabFormation = array(
+			"DUT INFO" => "diplome universitaire et technologique informatique",
+			"DUT GEA" => "diplome universitaire et technologique gestion des entrprises et administration"
+			"LP LA" => "licence professionnelle programation avancée"
+		);
+		
+		
+		foreach($tabFormation as $nomCour => $nomLong){
+			$formation = new Formation();
+			$formation->setNom($nomCour);
+			$formation->setNomComplet($nomLong);
+			$tabFormation[] = $entreprise;
+			$manager->persist($formation);
+		}
+		
+		
+		$manager->flush();
+		
+		foreach	($tabEntreprise as $ent){
+			$nbStage = $faker->numberBetween($min = 1, $max =5);
+			for($i = 0; $i < $nbStage ; $i++){
+				$stage = new Stage();
+				$stage->setTitre($faker->jobTitle);
+				$stage->setMission($faker->sentence($nbWords = 30 , $variableNbWords = true));
+				$stage->setEmailContact("$ent->getNom()@gmail.com");
+				
+			}
+			
     }
 }
